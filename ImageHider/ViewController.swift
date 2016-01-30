4c628fe1309239037386c1cff48da9a4843b7f2a
Eric Klose <ericklose@Erics-MacBook-Pro.local>
2016-01-30 12:23:33 -0700

    added files to git

--- ImageHider/ViewController.swift
+++ ImageHider/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var frontCoat: UIImageView!
+    @IBOutlet weak var BackCoat: UIImageView!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+//    @IBAction func toggleLeft(sender: UIButton) {
+//        if frontCoat.hidden == false {
+//        frontCoat.hidden = true
+//        } else {
+//            frontCoat.hidden = false
+//        }
+//    }
+    
+    @IBAction func toggleRight(sender: UIButton) {
+        if BackCoat.hidden == true {
+            frontCoat.hidden = true
+            BackCoat.hidden = false
+        } else {
+            BackCoat.hidden = true
+            frontCoat.hidden = false
+        }
+    }
 
 }
 
